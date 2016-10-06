61d688c40ad4df0fb49912a7efef387f490c1922
codeamac06 <codeamac06@gmail.com>
2016-10-06 12:21:14 -0500

    sin index

--- to_dos/app/models/task.rb
+++ to_dos/app/models/task.rb
@@ -1,7 +1,5 @@
 class Task < ActiveRecord::Base
-  def name
-    self.task
-  end
+
 # Fíjate en la línea anterior como este modelo está heredando de la clase
 # ActiveRecord::Base
 # Que tanto código te regala ActiveRecord
--- to_dos/db/migrate/20121011144238_create_tasks.rb
+++ to_dos/db/migrate/20121011144238_create_tasks.rb
@@ -2,7 +2,6 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :task
-      t.integer :index
       t.boolean :status
     end
   end
