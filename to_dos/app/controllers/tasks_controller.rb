f11701ee30b0e18c9bd9fd5117a6f39bb7411c07
codeamac06 <codeamac06@gmail.com>
2016-10-06 12:15:49 -0500

    funciona todos los metodos

--- to_dos/app/controllers/tasks_controller.rb
+++ to_dos/app/controllers/tasks_controller.rb
@@ -10,8 +10,12 @@ class TasksController
     case option
     when "index"
       index
-    when "create"
-      create
+    when "add"
+      add
+    when "delete"
+      delete
+    when "complete"
+        complete
     else
       @view.error
     end
@@ -19,15 +23,67 @@ class TasksController
 
   def index
     todo = Task.pluck(:task)
-    @view.index(todo)
+    todo_status = Task.pluck(:status)
+    @view.index(todo,todo_status)
   end
 
   def add
+    if @args[1] != nil
+      string = make_string
+      task = Task.new
+      task.task = string
+      task.index = 0
+      task.status = false
+      task.save
+      @view.add(string) 
+
+    else
+      @view.error
+    end
   end
 
   def delete
+    if @args[1] != nil
+      string = find_me
+      if string != nil
+        user = Task.find_by(task: string) 
+        user.destroy
+        @view.delete(string)
+      else
+        @view.error
+      end
+    else
+        @view.error
+    end
   end
 
-  def complete
+  def complete  
+    if @args[1] != nil
+      string = find_me
+      if string != nil
+        user = Task.find_by(task: string) 
+        user.status=true
+        user.save
+        @view.complete(string)
+      else
+        @view.error
+      end
+    else
+        @view.error
+    end
+  end
+
+  def make_string
+    string = ""
+    for i in 1..@args.size-1
+      x=@args[i]
+      string << "#{x} "
+    end
+    string.chop
+  end
+
+  def find_me
+    todo = Task.pluck(:task)
+    todo[@args[1].to_i-1]
   end
 end
--- to_dos/app/views/tasks_view.rb
+++ to_dos/app/views/tasks_view.rb
@@ -1,21 +1,26 @@
 class TasksView
 # Recuerda que la única responsabilidad de la vista es desplegar data al usuario
 
-	def index(thing)
-    count = 1
-    thing.each do |task|
-      print "#{count}. #{task}\n"
-      count+=1
+	def index(thing,status)
+    for i in 0..thing.size-1
+      if status[i]
+      print "#{i+1}. #{thing[i]} [x]\n"
+      else
+        print "#{i+1}. #{thing[i]} [ ]\n"
+      end
     end
   end
 
-  def create
+  def add(string)
+    print "You succesfully added '#{string}' to your tasks\n"
   end
 
-  def delete
+  def delete(string)
+  print "You succesfully deleted '#{string}' of your tasks\n"
   end
 
-  def update
+  def complete(string)
+    print "You succesfully completed '#{string}' of your tasks\n"
   end
 
 	def error
